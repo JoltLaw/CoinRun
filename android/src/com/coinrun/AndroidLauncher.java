71f375e31b2b9d30b845fd0804dbecfd4c1399f1
JoltLaw <joshua.rian04@gmail.com>
2023-05-22 12:00:35 -0600

    Init Commit

---
+++ android/src/com/coinrun/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package com.coinrun;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.coinrun.CoinRun;
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinRun(), config);
+	}
+}
